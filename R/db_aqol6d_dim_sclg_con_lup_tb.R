0824877eb49eb2c1c42f83e458a4740c37061e14
Matthew P Hamilton <matthew.hamilton@orygen.org.au>
2020-10-05 15:59:34 +1100

    Changed fn naming convention

---
+++ R/db_aqol6d_dim_sclg_con_lup_tb.R
@@ -0,0 +1,12 @@
+#' AQoL6D dimension scaling constants lookup table
+#' 
+#' Scaling constants for each dimension of AQoL6D.
+#' 
+#' A tibble
+#' 
+#' \describe{
+#'   \item{Dimension_chr}{Dimension (a character vector)}
+#'   \item{Constant_dbl}{Constant (a double vector)}
+#' }
+#' @source \url{https://www.aqol.com.au/index.php/scoring-algorithms}
+"aqol6d_dim_sclg_con_lup_tb"
