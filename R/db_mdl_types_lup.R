40e9b41a5567edd5ac228c42f6bde204311e7d74
Matthew P Hamilton <matthew.hamilton@orygen.org.au>
2020-11-05 17:58:55 +1100

    Attempted fix of output format arg

---
+++ R/db_mdl_types_lup.R
@@ -0,0 +1,11 @@
+#' Model types lookup table
+#' 
+#' A lookup table of abbreviations to describe the different model types supported by FBaqol functions
+#' 
+#' A tibble
+#' 
+#' \describe{
+#'   \item{short_name_chr}{Short name (a character vector)}
+#'   \item{long_name_chr}{Long name (a character vector)}
+#' }
+"mdl_types_lup"
