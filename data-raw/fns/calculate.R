8b9736f8ba15638abe22e04939a5ef1e28e54bef
Matthew Hamilton <matthew.hamilton@orygen.org.au>
2020-11-14 17:51:18 +1100

    More progress in abstracting initial regression tfn comparisons

--- data-raw/fns/calculate.R
+++ data-raw/fns/calculate.R
@@ -102,6 +102,36 @@ calculate_rmse_tfmn <- function(y_dbl,yhat_dbl){
   rmse_tfmn_dbl <- sqrt(mean((1-exp(-exp(yhat_dbl))-y_dbl)^2))
   return(rmse_tfmn_dbl)
 }
+calculate_dep_var_tfmn <- function(dep_var_val_dbl,
+                                  tfmn_1L_chr = "NTF",
+                                  tfmn_is_outp_1L_lgl = F){
+  tfd_dep_var_val_dbl <- dep_var_val_dbl
+if(tfmn_1L_chr == "LOG"){
+  if(tfmn_is_outp_1L_lgl)
+    tfd_dep_var_val_dbl <- exp(dep_var_val_dbl)
+  else
+    tfd_dep_var_val_dbl <- log(dep_var_val_dbl)
+}
+  if(tfmn_1L_chr == "LOGIT"){
+    if(tfmn_is_outp_1L_lgl)
+      tfd_dep_var_val_dbl <- boot::inv.logit(dep_var_val_dbl)
+    else
+      tfd_dep_var_val_dbl <- psych::logit(dep_var_val_dbl)
+  }
+  if(tfmn_1L_chr == "LOGLOG"){
+    if(tfmn_is_outp_1L_lgl)
+      tfd_dep_var_val_dbl <- exp(-exp(-dep_var_val_dbl))
+    else
+      tfd_dep_var_val_dbl <- -log(-log(dep_var_val_dbl))
+  }
+  if(tfmn_1L_chr == "CLL"){
+    if(tfmn_is_outp_1L_lgl)
+      tfd_dep_var_val_dbl <- 1-exp(-exp(dep_var_val_dbl))
+    else
+      tfd_dep_var_val_dbl <- log(-log(1-dep_var_val_dbl))
+  }
+  return(tfd_dep_var_val_dbl)
+}
 
 
 
--- data-raw/fns/transform.R
+++ data-raw/fns/transform.R
@@ -2,6 +2,13 @@ transform_dep_var_nm_for_cll <- function(dep_var_nm_1L_chr){
   tfd_dep_var_nm_1L_chr <- paste0(dep_var_nm_1L_chr,"_cloglog")
   return(tfd_dep_var_nm_1L_chr)
 }
+transform_dep_var_nm <- function(dep_var_nm_1L_chr,
+                                 tfmn_1L_chr = "NTF"){
+  tfd_dep_var_nm_1L_chr <- paste0(dep_var_nm_1L_chr,
+                                  ifelse(tfmn_1L_chr == "","",paste0("_",tfmn_1L_chr))
+                                  )
+  return(tfd_dep_var_nm_1L_chr)
+}
 transform_tb_to_mdl_inp <- function(data_tb,
                                     dep_var_nm_1L_chr = "aqol6d_total_w",
                                     predr_vars_nms_chr,
