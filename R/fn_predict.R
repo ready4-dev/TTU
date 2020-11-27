c2613adc4fb9eeaad9d8c7944ed833c6eb0cabc2
Matthew Hamilton <matthew.hamilton@orygen.org.au>
2020-11-27 22:33:14 +1100

    Increment version number

--- R/fn_predict.R
+++ R/fn_predict.R
@@ -1,30 +1,9 @@
-#' Predict from mdl coefs
-#' @description predict_from_mdl_coefs() is a Predict function that makes predictions from data using a specified statistical model. Specifically, this function implements an algorithm to predict from mdl coefs. The function returns Pred (a double vector).
-#' @param smry_of_mdl_tb Smry of mdl (a tibble)
-#' @param new_data_tb New data (a tibble)
-#' @return Pred (a double vector)
-#' @rdname predict_from_mdl_coefs
-#' @export 
-#' @importFrom dplyr filter pull
-#' @importFrom purrr map
-#' @importFrom stringr str_replace
-predict_from_mdl_coefs <- function (smry_of_mdl_tb, new_data_tb) 
-{
-    coef_tb <- smry_of_mdl_tb %>% dplyr::filter(!Parameter %in% 
-        c("R2", "RMSE", "Sigma"))
-    vecs_1_ls <- coef_tb$Parameter[-1] %>% purrr::map(~new_data_tb %>% 
-        dplyr::pull(.x %>% stringr::str_replace(" ", "_")) * 
-        coef_tb %>% dplyr::filter(Parameter == .x) %>% dplyr::pull(Estimate))
-    pred_dbl <- exp(Reduce(`+`, vecs_1_ls) + coef_tb %>% dplyr::filter(Parameter == 
-        "Intercept") %>% dplyr::pull(Estimate))
-    return(pred_dbl)
-}
 #' Predict utility
-#' @description predict_utility() is a Predict function that makes predictions from data using a specified statistical model. Specifically, this function implements an algorithm to predict utility. The function returns Predd Assessment of Quality of Life Six Dimension (a double vector).
+#' @description predict_utility() is a Predict function that makes predictions from data using a specified statistical model. Specifically, this function implements an algorithm to predict utility. The function returns Predd aqol6d (a double vector).
 #' @param data_tb Data (a tibble)
 #' @param tfmn_1L_chr Tfmn (a character vector of length one), Default: 'NTF'
 #' @param ... Additional arguments
-#' @return Predd Assessment of Quality of Life Six Dimension (a double vector)
+#' @return Predd aqol6d (a double vector)
 #' @rdname predict_utility
 #' @export 
 
--- data-raw/fns/fit.R
+++ data-raw/fns/fit.R
@@ -1,59 +1,20 @@
-fit_clg_log_tfmn <- function(data_tb,
-                             dep_var_nm_1L_chr = "aqol6d_total_w_cloglog",
-                             predr_vars_nms_chr,
-                             id_var_nm_1L_chr = "fkClientID",
-                             backend_1L_chr = getOption("brms.backend", "rstan"),
-                             iters_1L_int = 4000L,
-                             seed_1L_int = 1000L){
-  mdl_ls <- fit_ts_model_with_brm(data_tb,
-                                  dep_var_nm_1L_chr = dep_var_nm_1L_chr,
-                                  predr_vars_nms_chr = predr_vars_nms_chr,
-                                  link_1L_chr = "identity",
-                                  id_var_nm_1L_chr = id_var_nm_1L_chr,
-                                  backend_1L_chr = backend_1L_chr,
-                                  iters_1L_int = iters_1L_int,
-                                  seed_1L_int = seed_1L_int)
-  return(mdl_ls)
+fit_clg_log_tfmn <- function (data_tb, dep_var_nm_1L_chr = "aqol6d_total_w_cloglog", 
+    predr_vars_nms_chr, id_var_nm_1L_chr = "fkClientID", backend_1L_chr = getOption("brms.backend", 
+        "rstan"), iters_1L_int = 4000L, seed_1L_int = 1000L) 
+{
+    mdl_ls <- fit_ts_model_with_brm(data_tb, dep_var_nm_1L_chr = dep_var_nm_1L_chr, 
+        predr_vars_nms_chr = predr_vars_nms_chr, link_1L_chr = "identity", 
+        id_var_nm_1L_chr = id_var_nm_1L_chr, backend_1L_chr = backend_1L_chr, 
+        iters_1L_int = iters_1L_int, seed_1L_int = seed_1L_int)
+    return(mdl_ls)
 }
-fit_gsn_log_lnk <- function(data_tb,
-                            dep_var_nm_1L_chr = "aqol6d_total_w",
-                            predr_vars_nms_chr,
-                            id_var_nm_1L_chr = "fkClientID",
-                            backend_1L_chr = getOption("brms.backend", "rstan"),
-                            iters_1L_int = 4000L,
-                            seed_1L_int = 1000L){
-  mdl_ls <- fit_ts_model_with_brm(data_tb,
-                                  dep_var_nm_1L_chr = dep_var_nm_1L_chr,
-                                  predr_vars_nms_chr = predr_vars_nms_chr,
-                                  link_1L_chr = "log",
-                                  id_var_nm_1L_chr = id_var_nm_1L_chr,
-                                  backend_1L_chr = backend_1L_chr,
-                                  iters_1L_int = iters_1L_int,
-                                  seed_1L_int = seed_1L_int)
-  return(mdl_ls)
-}
-fit_ts_model_with_brm <- function(data_tb,
-                                  dep_var_nm_1L_chr,
-                                  predr_vars_nms_chr,
-                                  id_var_nm_1L_chr,
-                                  backend_1L_chr = getOption("brms.backend", "rstan"),
-                                  link_1L_chr = "identity",
-                                  iters_1L_int = 4000L,
-                                  seed_1L_int = 1000L){
-  mdl_ls <- brms::brm(formula = as.formula(paste0(dep_var_nm_1L_chr,#"aqol6d_total_w_cloglog",
-                                                  " ~ ",
-                                                  purrr::map_chr(predr_vars_nms_chr,
-                                                                 ~ paste0(.x,
-                                                                          "_baseline + ",
-                                                                          .x,"_change + ")) %>%
-                                                    paste0(collapse = ""),
-                                                  "(1|",
-                                                  id_var_nm_1L_chr,
-                                                  ")")),
-                      backend = backend_1L_chr,
-                      data = data_tb,
-                      family = gaussian(link=link_1L_chr),
-                      iter = iters_1L_int,
-                      seed = seed_1L_int)
-  return(mdl_ls)
+fit_gsn_log_lnk <- function (data_tb, dep_var_nm_1L_chr = "aqol6d_total_w", predr_vars_nms_chr, 
+    id_var_nm_1L_chr = "fkClientID", backend_1L_chr = getOption("brms.backend", 
+        "rstan"), iters_1L_int = 4000L, seed_1L_int = 1000L) 
+{
+    mdl_ls <- fit_ts_model_with_brm(data_tb, dep_var_nm_1L_chr = dep_var_nm_1L_chr, 
+        predr_vars_nms_chr = predr_vars_nms_chr, link_1L_chr = "log", 
+        id_var_nm_1L_chr = id_var_nm_1L_chr, backend_1L_chr = backend_1L_chr, 
+        iters_1L_int = iters_1L_int, seed_1L_int = seed_1L_int)
+    return(mdl_ls)
 }
