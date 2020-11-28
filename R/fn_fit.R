b2e05b6b0026f65ae85c052a798f35d53521dc46
Matthew Hamilton <matthew.hamilton@orygen.org.au>
2020-11-29 09:36:39 +1100

    fixing build errors

--- R/fn_fit.R
+++ R/fn_fit.R
@@ -11,6 +11,7 @@
 #' @rdname fit_clg_log_tfmn
 #' @export 
 
+#' @keywords internal
 fit_clg_log_tfmn <- function (data_tb, dep_var_nm_1L_chr = "aqol6d_total_w_cloglog", 
     predr_vars_nms_chr, id_var_nm_1L_chr = "fkClientID", backend_1L_chr = getOption("brms.backend", 
         "rstan"), iters_1L_int = 4000L, seed_1L_int = 1000L) 
@@ -34,6 +35,7 @@ fit_clg_log_tfmn <- function (data_tb, dep_var_nm_1L_chr = "aqol6d_total_w_clogl
 #' @rdname fit_gsn_log_lnk
 #' @export 
 
+#' @keywords internal
 fit_gsn_log_lnk <- function (data_tb, dep_var_nm_1L_chr = "aqol6d_total_w", predr_vars_nms_chr, 
     id_var_nm_1L_chr = "fkClientID", backend_1L_chr = getOption("brms.backend", 
         "rstan"), iters_1L_int = 4000L, seed_1L_int = 1000L) 
