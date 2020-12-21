afd4b3d7677e5dfad83533a3f1613a2ff607e4c7
Matthew Hamilton <matthew.hamilton@orygen.org.au>
2020-12-22 08:06:00 +1100

    update for dependencies@

--- R/fn_print.R
+++ R/fn_print.R
@@ -25,30 +25,41 @@ print_all_plts_for_mdl_set <- function (output_ls, start_from_1L_int = 0L)
 #' @param merge_row_idx_int Merge row index (an integer vector), Default: NA
 #' @param digits_dbl Digits (a double vector), Default: NULL
 #' @param big_mark_1L_chr Big mark (a character vector of length one), Default: ' '
-#' @param label PARAM_DESCRIPTION
-#' @param hline.after PARAM_DESCRIPTION
-#' @param addtorow PARAM_DESCRIPTION
+#' @param use_lbls_as_col_nms_1L_lgl Use lbls as column names (a logical vector of length one), Default: F
+#' @param scroll_box_args_ls Scroll box arguments (a list), Default: NULL
+#' @param mkdn_tbl_ref_1L_chr Mkdn table reference (a character vector of length one)
+#' @param hline_after_ls Hline after (a list), Default: NULL
+#' @param add_to_row_ls Add to row (a list), Default: NULL
 #' @param sanitize_fn Sanitize (a function), Default: getOption("xtable.sanitize.text.function", NULL)
 #' @return NULL
 #' @rdname print_table_xx
 #' @export 
+#' @importFrom Hmisc label
+#' @importFrom dplyr rename_with
 #' @importFrom xtable xtable
-#' @importFrom kableExtra kbl kable_paper row_spec footnote
+#' @importFrom kableExtra kbl kable_paper row_spec footnote scroll_box
+#' @importFrom rlang exec
 #' @importFrom flextable flextable set_caption merge_h_range bold colformat_num autofit add_footer_lines
 #' @importFrom officer run_autonum
 #' @importFrom purrr reduce
 #' @keywords internal
 print_table_xx <- function (data_tb, output_type_1L_chr = "PDF", caption_1L_chr = NA_character_, 
     footnotes_chr = NA_character_, merge_row_idx_int = NA_integer_, 
-    digits_dbl = NULL, big_mark_1L_chr = " ", label, hline.after, 
-    addtorow, sanitize_fn = getOption("xtable.sanitize.text.function", 
+    digits_dbl = NULL, big_mark_1L_chr = " ", use_lbls_as_col_nms_1L_lgl = F, 
+    scroll_box_args_ls = NULL, mkdn_tbl_ref_1L_chr, hline_after_ls = NULL, 
+    add_to_row_ls = NULL, sanitize_fn = getOption("xtable.sanitize.text.function", 
         NULL)) 
 {
+    if (use_lbls_as_col_nms_1L_lgl & !any(Hmisc::label(data_tb) == 
+        "")) {
+        data_tb <- data_tb %>% dplyr::rename_with(~Hmisc::label(data_tb)[names(Hmisc::label(data_tb)) == 
+            .x])
+    }
     if (output_type_1L_chr == "PDF") {
         data_x_tb <- data_tb %>% xtable::xtable(caption = caption_1L_chr, 
-            label = label, digits = digits_dbl)
-        data_x_tb %>% print(comment = F, floating = TRUE, hline.after = hline.after, 
-            caption.placement = "top", add.to.row = addtorow, 
+            label = mkdn_tbl_ref_1L_chr, digits = digits_dbl)
+        data_x_tb %>% print(comment = F, floating = TRUE, hline.after = hline_after_ls, 
+            caption.placement = "top", add.to.row = add_to_row_ls, 
             sanitize.text.function = sanitize_fn, format.args = list(big.mark = big_mark_1L_chr), 
             include.colnames = FALSE, include.rownames = FALSE)
     }
@@ -62,8 +73,14 @@ print_table_xx <- function (data_tb, output_type_1L_chr = "PDF", caption_1L_chr 
                 data_kb <- data_kb %>% kableExtra::row_spec(merge_row_idx_int, 
                   bold = T)
             }
-            data_kb <- data_kb %>% kableExtra::footnote(general = footnotes_chr, 
-                general_title = "", footnote_as_chunk = T, threeparttable = T)
+            data_kb <- data_kb %>% kableExtra::footnote(general = ifelse(is.na(footnotes_chr[1]), 
+                "", footnotes_chr), general_title = "", footnote_as_chunk = T, 
+                threeparttable = T)
+            if (!is.null(scroll_box_args_ls)) {
+                data_kb <- data_kb %>% kableExtra::kable_paper()
+                data_kb <- rlang::exec(kableExtra::scroll_box, 
+                  data_kb, !!!scroll_box_args_ls)
+            }
             data_xx <- data_kb
         }
         if (output_type_1L_chr == "Word") {
