33e92952fddc6e3fa15b02b1966340394d0683c8
Matthew Hamilton <matthew.hamilton@orygen.org.au>
2021-05-18 13:50:35 +1000

    fake data edits to rmds

--- data-raw/fns/get.R
+++ data-raw/fns/get.R
@@ -5,5 +5,7 @@ get_signft_covars <- function (mdls_with_covars_smry_tb, covar_var_nms_chr)
     purrr::flatten_chr() %>% unique()
   signt_covars_chr <- covar_var_nms_chr[covar_var_nms_chr %in%
                                           signif_vars_chr]
+  if(identical(signt_covars_chr, character(0)))
+    signt_covars_chr <- NA_character_
   return(signt_covars_chr)
 }
