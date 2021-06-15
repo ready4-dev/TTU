c9a7d5f60cd93c3726337e297e17c548cedae3e1
Matthew Hamilton <matthew.hamilton@orygen.org.au>
2021-06-15 13:30:16 +1000

    fix of arg in knit pars ls fn

--- R/fn_knit.R
+++ R/fn_knit.R
@@ -10,13 +10,13 @@
 #' @keywords internal
 knit_mdl_rprt <- function (knit_pars_ls, path_to_mdl_rprt_tmpl_1L_chr) 
 {
-    src <- purrr::pmap(knit_pars_ls, ~knitr::knit_expand(path_to_mdl_rprt_tmpl_1L_chr, 
-        plt_nms_chr = ..1 %>% deparse(), path_to_mdl_1L_chr = ..2 %>% 
-            deparse(), caption_1L_chr = ..3 %>% deparse(), label_stub_1L_chr = ..4 %>% 
-            deparse(), output_type_1L_chr = ..5 %>% deparse(), 
-        section_ttl_1L_chr = ..6, paths_to_mdl_plts_chr = ..7 %>% 
-            deparse(), mdl_nms_chr = names(knit_pars_ls$plt_nms_ls) %>% 
-            deparse()))
+    src <- purrr::pmap(append(knit_pars_ls, list(names(knit_pars_ls$plt_nms_ls))), 
+        ~knitr::knit_expand(path_to_mdl_rprt_tmpl_1L_chr, plt_nms_chr = ..1 %>% 
+            deparse(), path_to_mdl_1L_chr = ..2 %>% deparse(), 
+            caption_1L_chr = ..3 %>% deparse(), label_stub_1L_chr = ..4 %>% 
+                deparse(), output_type_1L_chr = ..5 %>% deparse(), 
+            section_ttl_1L_chr = ..6, paths_to_mdl_plts_chr = ..7 %>% 
+                deparse(), mdl_nms_chr = ..8 %>% deparse()))
     res <- knitr::knit_child(text = unlist(src), quiet = TRUE)
     cat(res, sep = "\n")
 }
