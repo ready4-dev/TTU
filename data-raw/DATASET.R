69191bf6519bac989d63bcdb75a84caae586274a
Matthew Hamilton <matthew.hamilton@orygen.org.au>
2021-06-16 18:06:10 +1000

    updated vignettes

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -230,6 +230,7 @@ usethis::use_build_ignore("initial_setup.R")
 readLines(".github/workflows/R-CMD-check.yaml")[-28] %>%
   writeLines(".github/workflows/R-CMD-check.yaml")
 usethis::use_package("betareg")
+usethis::use_package("eq5d")
 usethis::use_package("ggfortify")
 usethis::use_dev_package("cmdstanr")
 usethis::use_package("knitrBootstrap")
