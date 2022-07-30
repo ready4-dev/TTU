1a48ecb89ba09efcdfa43e1a06bb360d12077813
Matthew Hamilton <Matthew.Hamilton@orygen.org.au>
2022-07-30 19:08:41 +1000

    more wip for new TTU class

--- R/C4_TTUProject.R
+++ R/C4_TTUProject.R
@@ -2,10 +2,10 @@
 #' 
 #' Input And Output Data For Undertaking and Reporting Utility Mapping Studies.
 #' 
-#' @include C4_TTUSynopsis.R C4_TTUReports.R
+#' @include C4_TTUReports.R
 #' @slot a_ScorzProfile  (an instance of the ScorzProfile class)
 #' @slot b_SpecificParameters  (an instance of the SpecificParameters class)
-#' @slot c_TTUSynopsis  (an instance of the TTUSynopsis class)
+#' @slot c_SpecificProject  (an instance of the SpecificProject class)
 #' @slot d_TTUReports  (an instance of the TTUReports class)
 #' @slot dissemination_1L_chr Dissemination (a character vector of length one)
 #' @import ready4
@@ -15,8 +15,8 @@
 #' @exportClass TTUProject
 TTUProject <- methods::setClass("TTUProject",
 contains = "Ready4Module",
-slots = c(a_ScorzProfile = "ScorzProfile",b_SpecificParameters = "SpecificParameters",c_TTUSynopsis = "TTUSynopsis",d_TTUReports = "TTUReports",dissemination_1L_chr = "character"),
-prototype =  list(a_ScorzProfile = scorz::ScorzProfile(),b_SpecificParameters = specific::SpecificParameters(),c_TTUSynopsis = TTUSynopsis(),d_TTUReports = TTUReports()))
+slots = c(a_ScorzProfile = "ScorzProfile",b_SpecificParameters = "SpecificParameters",c_SpecificProject = "SpecificProject",d_TTUReports = "TTUReports",dissemination_1L_chr = "character"),
+prototype =  list(a_ScorzProfile = scorz::ScorzProfile(),b_SpecificParameters = specific::SpecificParameters(),c_SpecificProject = specific::SpecificProject(),d_TTUReports = TTUReports()))
 
 
 methods::setValidity(methods::className("TTUProject"),
